abc146831d8a18dfd73b012be27821068d3053cd
Cloud474 <huseyin.blt47@hotmail.com>
2022-08-31 14:17:32 +0300

    New GWD

--- src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
+++ src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@Regression",
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
-        plugin = {"html:target//cucumber-reports.html"}
+
+        plugin = { "html:target//cucumber-reports.html" }
 )
 public class _06_TestRunnerRegressionWithPlugin extends AbstractTestNGCucumberTests {
 }

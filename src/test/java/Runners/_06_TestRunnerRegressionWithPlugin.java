ce3481dd5df0cae83b2db1d32ab77941cdbc2b5d
Cloud474 <huseyin.blt47@hotmail.com>
2022-10-13 15:48:24 +0300

    For jenkins POM code change

--- src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
+++ src/test/java/Runners/_06_TestRunnerRegressionWithPlugin.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
        // plugin = {"html:target//cucumber-reports.html"}
-        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+        plugin = {"html:target//cucumber-reports.html"}
 )
 public class _06_TestRunnerRegressionWithPlugin extends AbstractTestNGCucumberTests {
 
